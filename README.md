4cb0cd6e91f31c9d93d929cf540698e1283f445b
Jimmy <b00502013@gmail.com>
2024-02-11 05:54:52 +0800

    0.0.1

--- README.md
+++
@@ -1,8 +0,0 @@
-# Put a `gitas-config.json` file in `~`
-
-```json
-[
-  { "email": "b00502013@gmail.com", "name": "Jimmy Lin" },
-  { "email": "foo@bar.com", "name": "baz" }
-]
-```
--- index.js
+++ index.js
@@ -2,9 +2,29 @@
 
 import fs from "fs";
 import path from "path";
+import { exec } from "child_process";
 
-const configFilePath = path.join(process.env.HOME, "gitas-config.json");
+const filePath = path.join(process.env.HOME, "gitas-config.json");
+const content = fs.readFileSync(filePath, "utf8");
+const configs = JSON.parse(content);
+const config = configs.find((config) => config.email === process.argv[2]);
+const { email, name } = config;
 
-const data = fs.readFileSync(configFilePath, "utf8");
-const config = JSON.parse(data);
-console.log(config);
+const commands = [
+  `git config --global user.email ${email}`,
+  `git config --global user.name ${name}`,
+  `rm ~/.ssh/id_ed25519`,
+  `rm ~/.ssh/id_ed25519.pub`,
+  `cp ~/.ssh/${email}/id_ed25519 ~/.ssh/id_ed25519`,
+  `cp ~/.ssh/${email}/id_ed25519 ~/.ssh/id_ed25519.pub`,
+];
+
+exec(commands.join(";"), (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+  } else if (stderr) {
+    console.log(`stderr: ${stderr}`);
+  } else if (stdout) {
+    console.log(`stdout: ${stdout}`);
+  }
+});
